99a9b104c5408a4163c376c6adde15af2a75c4df
Omer <omerdavid97@gmail.com>
2020-11-23 09:46:05 +0200

    child process python

---
+++ backend/process/pythonML.js
@@ -0,0 +1,13 @@
+const {spawn} = require('child_process');
+const python = spawn('python', ['script1.py']);
+let data = '';
+ // collect data from script
+python.stdout.on('data', function (data) {
+    console.log('Pipe data from python script ...');
+    dataToSend = data.toString();
+});
+// in close event we are sure that stream from child process is closed
+python.on('close', (code) => {
+console.log(`child process close all stdio with code ${code}`);
+ // send data to browser
+});
