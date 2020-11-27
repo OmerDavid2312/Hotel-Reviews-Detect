d10842e30d507cfc46a9bcadaa1c38c3257f4c22
Omer <omerdavid97@gmail.com>
2020-11-27 21:29:19 +0200

    hotels in city

--- frontend/hotels-reviews-detector/src/app/components/hotels/hotels/hotels.component.ts
+++ frontend/hotels-reviews-detector/src/app/components/hotels/hotels/hotels.component.ts
@@ -3,6 +3,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HotelsService } from 'src/app/services/hotels.service';
 import { Subscription } from 'rxjs';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-hotels',
@@ -13,20 +14,25 @@ export class HotelsComponent implements OnInit,OnDestroy{
 
   hotels:Hotel[];
   cityName:string;
+  isFetched:boolean = false;
   private subscription$: Subscription;
 
-  constructor(private route:ActivatedRoute,private hotelsSrv:HotelsService,private router:Router) { }
+  constructor(private route:ActivatedRoute,private hotelsSrv:HotelsService,private router:Router,private spinner:NgxSpinnerService) { }
 
   ngOnInit() {
     if(!this.route.snapshot.paramMap.get('cityName')){
       this.router.navigateByUrl('/'); //back to cities page
     }
     this.cityName = this.route.snapshot.paramMap.get('cityName');
-
+    this.spinner.show();
     this.subscription$ =this.hotelsSrv.getCityHotels(this.cityName).subscribe(hotels=>{
+      this.spinner.hide();
+      this.isFetched = true;
       this.hotels = hotels;
       console.log(this.hotels);
       
+    },err=>{
+      this.spinner.hide();
     })
 
   }
