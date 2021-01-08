80a61c58df166b14186455251a5d91e1d44c1442
Omer <omerdavid97@gmail.com>
2021-01-08 22:26:15 +0200

    update mongo

--- backend/configs/mongo.js
+++ backend/configs/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 module.exports = {
     connectMongo: ()=>{
-        mongoose.connect(process.env.MONGO,{useNewUrlParser:true,useUnifiedTopology: true})
+        mongoose.connect(process.env.MONGO,{useNewUrlParser:true,useUnifiedTopology: true}) 
             .then(() => {
                 console.log("Connected to Mongo database!");
             })
