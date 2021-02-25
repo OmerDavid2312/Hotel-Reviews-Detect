1b579cadc3d7a4f60293ffe72c022f343aaebc79
Omer <omerdavid97@gmail.com>
2021-02-25 19:38:47 +0200

    redis cloud

--- backend/configs/redis.js
+++ backend/configs/redis.js
@@ -1,19 +1,22 @@
-const redis = require("redis");
-const client = redis.createClient({
-    host: '127.0.0.1',
-    port: 6379
+let createClient = require("redis").createClient;
+
+let client = createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
 });
 
-client.on('connect', () => {
-    console.log('Connected to Redis database');
+client.auth(process.env.REDIS_PASSWORD);
+
+client.on("connect", () => {
+  console.log("Connected to Redis database");
 });
 client.on("error", (error) => {
-    console.error('Connection to Redis database failed! ==>' + error);
+  console.error("Connection to Redis database failed! ==>" + error);
 });
 
 module.exports = {
-    client:client,
-    setCache:(key,seconds,value)=>{
-        client.setex(key,seconds,value);
-    }
+  client: client,
+  setCache: (key, seconds, value) => {
+    client.setex(key, seconds, value);
+  },
 };
