cf4bf6062a4829949118a6306d40592c6c3e5684
Omer <omerdavid97@gmail.com>
2021-02-26 20:42:35 +0200

    migration script

--- scrapper/data/Mongo.js
+++ scrapper/data/Mongo.js
@@ -1,30 +1,37 @@
-const mongoose = require('mongoose');
-const Logger = require('../utils/Logger');
-const Model = mongoose.model('hotel', new mongoose.Schema({},{strict: false}));
+const mongoose = require("mongoose");
+const Logger = require("../utils/Logger");
+const Model = mongoose.model(
+  "hotel",
+  new mongoose.Schema({}, { strict: false })
+);
 
-class Mongo{
-    static async connectDB(){
-        await mongoose.connect('mongodb://localhost/biuFinalProj',{useNewUrlParser:true,useUnifiedTopology: true});
-    }   
-    static async storeData(dataObject){
-        if(typeof dataObject === 'object'){
-            try {
-                let key = Object.keys(dataObject)[0];
-                const isExisted = await Model.findOne({[key]:dataObject[key]})
-                if(!isExisted){
-                        const doc = await new Model(dataObject);
-                        const savedDoc = await doc.save();
-                        Logger.info(savedDoc);
-                 }
-            } catch (error) {
-                Logger.error(error);
-            }
-            finally{
-                Logger.log(`finished store data! -- ${dataObject[Object.keys(dataObject)[0]]}`)
-            }
-        } else {
-            Logger.error('invalid Params');
+class Mongo {
+  static async connectDB() {
+    await mongoose.connect("mongodb://localhost/biuFinalProj", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+  static async storeData(dataObject) {
+    if (typeof dataObject === "object") {
+      try {
+        let key = Object.keys(dataObject)[0];
+        const isExisted = await Model.findOne({ [key]: dataObject[key] });
+        if (!isExisted) {
+          const doc = await new Model(dataObject);
+          const savedDoc = await doc.save();
+          Logger.info(savedDoc);
         }
+      } catch (error) {
+        Logger.error(error);
+      } finally {
+        Logger.log(
+          `finished store data! -- ${dataObject[Object.keys(dataObject)[0]]}`
+        );
+      }
+    } else {
+      Logger.error("invalid Params");
     }
+  }
 }
-module.exports = {Mongo,Model};
+module.exports = { Mongo, Model };
--- scrapper/utils/Utils.js
+++ scrapper/utils/Utils.js
@@ -1,98 +1,138 @@
-const fetch = require('node-fetch');
-const xl = require('excel4node');
-const Logger = require('./Logger');
-const fs = require('fs');
-const { Model, Mongo } = require('../data/Mongo');
+const fetch = require("node-fetch");
+const xl = require("excel4node");
+const Logger = require("./Logger");
+const fs = require("fs");
+const { Model, Mongo } = require("../data/Mongo");
 
-class Utils{
-    static async getHTML(url){
-        try {
-            const response = await fetch(url);
-            if(response.status === 200){
-                return await response.text();
-            } else {
-                throw 'Faild get html, Status code: ' + response.status;
-            }
-        } catch (error) {
-            Logger.error(error);
-        }
-
+class Utils {
+  static async getHTML(url) {
+    try {
+      const response = await fetch(url);
+      if (response.status === 200) {
+        return await response.text();
+      } else {
+        throw "Faild get html, Status code: " + response.status;
+      }
+    } catch (error) {
+      Logger.error(error);
     }
-    static async saveJson(object){
-        try {
-            return new Promise((resolve,reject)=>{
-                fs.writeFile('./data.json',JSON.stringify(object),'utf-8',(err)=>{
-                    err ? reject('can not save json') : resolve()
-                })
-            })   
-        } catch (error) {
-            Logger.error(error);
-        }
+  }
+  static async saveJson(object) {
+    try {
+      return new Promise((resolve, reject) => {
+        fs.writeFile("./data.json", JSON.stringify(object), "utf-8", (err) => {
+          err ? reject("can not save json") : resolve();
+        });
+      });
+    } catch (error) {
+      Logger.error(error);
     }
-    static async saveExcel(object){
-        return new Promise((resolve,reject)=>{
-            try {
-                const wb = new xl.Workbook();
-                const ws = wb.addWorksheet('hotels reviews');
-                const headingColumnNames = [
-                    "name",
-                    "title",
-                    "desc",
-                    "user"
-                ]
-                let headingColumnIndex = 1;
-                headingColumnNames.forEach(heading => {
-                    ws.cell(1, headingColumnIndex++)
-                        .string(heading)
-                });
-                let rowIndex = 2;
-                object.forEach( record => {
-                    let columnIndex = 1;
-                    Object.keys(record).forEach(columnName =>{
-                        ws.cell(rowIndex,columnIndex++)
-                            .string(record [columnName])
-                    });
-                    rowIndex++;
-                });
-                wb.write('hotelReviews.csv');
-                resolve();   
-            } catch (error) {
-                reject(error);
-            }
-        })
-    }
-
+  }
+  static async saveExcel(object) {
+    return new Promise((resolve, reject) => {
+      try {
+        const wb = new xl.Workbook();
+        const ws = wb.addWorksheet("hotels reviews");
+        const headingColumnNames = ["name", "title", "desc", "user"];
+        let headingColumnIndex = 1;
+        headingColumnNames.forEach((heading) => {
+          ws.cell(1, headingColumnIndex++).string(heading);
+        });
+        let rowIndex = 2;
+        object.forEach((record) => {
+          let columnIndex = 1;
+          Object.keys(record).forEach((columnName) => {
+            ws.cell(rowIndex, columnIndex++).string(record[columnName]);
+          });
+          rowIndex++;
+        });
+        wb.write("hotelReviews.csv");
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 }
 
-// const updateReviewsPrediction =  async ()=>{
-//     let index = 0;
-//     try {
-//         const json = require('./i12or-bqhs6.json');
-//          await Mongo.connectDB();
-//          json.forEach(async record=>{
-//          await Model.findOneAndUpdate({name:record.name,"reviews.user":record.user},
-//         {$set: {"reviews.$.reliability": record.reliability}});
-//     })        
-//     } catch (error) {
-//         console.log(error);
-//     }finally{
-//         console.log('done!!');
-//     }
+//remainder!!!!
+///
+///
+// BEFORE CONNECT TO MONGO MAKE SURE U CONNECT THE CORRECT ONE !!!!
+//
+//
+// const updateDBForCategories = async () => {
+//   index = 0;
+//   try {
+//     const CATEGORIES = [
+//       "service",
+//       "price",
+//       "room",
+//       "location",
+//       "food",
+//       "cleanliness",
+//     ];
+//     const json = require("./convertcsv.json");
+//     const NEWJSON = [];
+//     await Mongo.connectDB();
+//     json.forEach((record) => {
+//       CATEGORIES.includes(record["I"])
+//         ? (record["I"] = record["I"])
+//         : (record["I"] = "general");
+//       CATEGORIES.includes(record["J"])
+//         ? (record["J"] = record["J"])
+//         : (record["J"] = "general");
+//       CATEGORIES.includes(record["K"])
+//         ? (record["K"] = record["K"])
+//         : (record["K"] = "general");
+//       CATEGORIES.includes(record["L"])
+//         ? (record["L"] = record["L"])
+//         : (record["L"] = "general");
+//       CATEGORIES.includes(record["M"])
+//         ? (record["M"] = record["M"])
+//         : (record["M"] = "general");
 
-// }
+//       NEWJSON.push({
+//         D: record["D"],
+//         G: record["G"],
+//         categories: [
+//           record["I"],
+//           record["J"],
+//           record["K"],
+//           record["L"],
+//           record["M"],
+//         ],
+//       });
+//     });
+
+//     for await (const record of NEWJSON) {
+//       console.log(index++);
+//       await Model.findOneAndUpdate(
+//         { name: record["D"], "reviews.user": record["G"] },
+//         { $set: { "reviews.$.categories": record.categories } }
+//       );
+//       //   await new Promise((res) => setTimeout(async () => res(), 200));
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   } finally {
+//     console.log("done!!");
+//   }
+// };
+// updateDBForCategories();
 // updateReviewsPrediction()
 // const saveReviewsExcel = async ()=>{
 //     try {
 //         let json = []
 //         await Mongo.connectDB();
-//         let results = await Model.find({},{_id:0,reviews:1,name:1}); 
+//         let results = await Model.find({},{_id:0,reviews:1,name:1});
 //         results.forEach(res=>{
 //             let result = res._doc;
 //             result.reviews.forEach(review=>{
 //                 json.push(Object.assign({name:result.name},{title:review.title,desc:review.desc,user:review.user}));
 //             })
 //         })
-//         await Utils.saveExcel(json);  
+//         await Utils.saveExcel(json);
 //     } catch (error) {
 //         Logger.error(error)
 //     }finally{
@@ -100,4 +140,4 @@ class Utils{
 //     }
 // }
 // saveReviewsExcel()
-module.exports =  Utils
+module.exports = Utils;
